6bc295981e1d5a8c55999e0de7101ca82813d3a4
Thomas <thomas.mccabe.44@gmail.com>
2020-05-13 14:00:12 -0400

    Updates

---
+++ precache-manifest.87891bac58f74eebc4b66ec24d862610.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b2d25322627e2f33c5dd73c5e2467b",
+    "url": "/Divisions-Teams/index.html"
+  },
+  {
+    "revision": "0d97f4d54c5928e59c50",
+    "url": "/Divisions-Teams/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1288607468fd0be2eb32",
+    "url": "/Divisions-Teams/static/css/main.67725c65.chunk.css"
+  },
+  {
+    "revision": "0d97f4d54c5928e59c50",
+    "url": "/Divisions-Teams/static/js/2.14d73260.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Divisions-Teams/static/js/2.14d73260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1288607468fd0be2eb32",
+    "url": "/Divisions-Teams/static/js/main.d074624b.chunk.js"
+  },
+  {
+    "revision": "fb671b87b94857a13097",
+    "url": "/Divisions-Teams/static/js/runtime-main.4c750254.js"
+  },
+  {
+    "revision": "1b0527279b785237fe01c7eb087c428f",
+    "url": "/Divisions-Teams/static/media/ModernWarfare-8MM6z.1b052727.woff"
+  }
+]);
